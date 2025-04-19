497046723aedbd6ce337983c6fca5c3dd9c8e658
Cherryshine <hi.im.cherryshine@gmail.com>
2025-04-19 20:24:30 +0900

    commit

--- install_pyenv_on_debian.sh
+++ install_pyenv_on_debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 필수 패키지 설치
-sudo apt update && sudo apt install -y curl git build-essential libssl-dev zlib1g-dev \
+apt update && apt install -y curl git build-essential libssl-dev zlib1g-dev \
   libbz2-dev libreadline-dev libsqlite3-dev wget llvm libncursesw5-dev xz-utils \
   tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev ca-certificates
 
@@ -14,8 +14,11 @@ echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 
-# 환경 변수 적용
-source ~/.bashrc
+# 환경 변수 현재 세션에 바로 적용
+export PATH="/root/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 # Python 3.12 설치
 pyenv install 3.12.0
